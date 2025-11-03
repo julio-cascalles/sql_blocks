9a0bedae52010afd2b3ce02208a4e24bb0e3bd33
Júlio Cascalles <julio.cascalles@outlook.com>
2025-11-03 19:52:58 -0300

    [chore] Version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'sql_blocks',
-    version = '1.20251103',
+    version = '1.20251106',
     author = 'Júlio Cascalles',
     author_email = 'julio.cascalles@outlook.com',
     packages = ['sql_blocks'],
